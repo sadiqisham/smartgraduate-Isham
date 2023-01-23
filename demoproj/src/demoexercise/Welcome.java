1530a7d4618fdbe6dc8a3097d1019d703e9feb57
isham <112255717+sadiqisham@users.noreply.github.com>
2023-01-23 22:38:19 +0530

    Commit head first

---
+++ demoproj/src/demoexercise/Welcome.java
@@ -0,0 +1,8 @@
+package demoexercise;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to java EE version");
+	}
+
+}
---
+++ demoproj/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author my pc
+ *
+ */
+module demoproj {
+}
